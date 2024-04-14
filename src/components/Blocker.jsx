2e14f76bb7d6bb6ae9089b508b4cc78075d1967d
Sean <63425198+seanjulagay@users.noreply.github.com>
2024-04-14 14:45:04 +0800

    Added more guide text for mobile

--- src/components/Blocker.jsx
+++ src/components/Blocker.jsx
@@ -13,6 +13,20 @@ export default function Blocker() {
           Please rotate your device to a landscape orientation.
           <br />
           <img src={rotateIcon} alt="" className="rotate-icon" />
+          <br />
+          <br />
+          If it won't rotate, please select "open in browser" in the settings.
+          <br />
+          <br />
+          Or open your dedicated browser and enter
+          <br />
+          <a
+            href="https://seans-pokedex.netlify.app/"
+            target="_blank"
+            className="link-text"
+          >
+            seans-pokedex.netlify.com
+          </a>
           {/* <br />
           If you're on PC, please resize your window to have a longer width than
           height. */}
